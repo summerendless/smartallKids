c67bb75e5c93118b6c180e7e28ee1846ecd8a8a3
phj09 <hj0909@onflou.com>
2023-06-09 18:32:21 +0900

    20230609

--- COMM/JS/COLOR.js
+++ COMM/JS/COLOR.js
@@ -1,14 +1,23 @@
 const colorOption = Array.from(document.getElementsByClassName("color-option")); /** 자바스크립트에서 배열로 바꾸어 html 클래스를 부른다. */
 const color = document.getElementById("color") /** querySelector("#color")와 동일하다. */
 const lineWidth = document.querySelector("#line-width") /** html에서 선굵기 조절할 input id 값을 lineWidth에 지정 */
-const canvas = document.querySelector("canvas"); /** canvas라는 변수는 html의 canvas라는 엘리먼트를 지정한다 */
-
+const canvas = document.querySelector(".canvas1"); /** canvas라는 변수는 html의 canvas라는 엘리먼트를 지정한다 */
+const canvasImg = document.querySelector(".canvas2"); /** canvas라는 변수는 html의 canvas라는 엘리먼트를 지정한다 */
 
 const ctx = canvas.getContext("2d"); /** 2d로 그릴 것을지정 ctx는 canvas에 그림 그릴 때 쓰는 것  */
-canvas.width = 800;
-canvas.height = 800;
+const ctxImg = canvasImg.getContext("2d"); /** 2d로 그릴 것을지정 ctx는 canvas에 그림 그릴 때 쓰는 것  */
+canvas.width = 400;
+canvas.height = 900;
 ctx.lineWidth = lineWidth.value; /** 기본 선 굵기 값 설정 */
 let isPainting = false; /** isPainting을 true로 변경될 수 있도록 let으로 설정한다. */
+
+const image = new Image();
+image.src = '../../COMM/IMG/WORD/1_1/WORD_1_1_1.PNG';
+image.onload = () => {
+  ctxImg.drawImage(image, 4, 4)
+}
+// drawImage()
+ctx.globalCompositeOperation = "source-atop";
 
 function onMove(event){
   if(isPainting){ /** isPainting이 true면 */
